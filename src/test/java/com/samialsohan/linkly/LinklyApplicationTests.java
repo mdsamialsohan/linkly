3b29e8a1ce3001cd99cf357cf8ecf51ab4fa39ab
samialsohan <samialsohan@gmail.com>
2026-05-14 00:08:11 +0100

    Intialise spring boot scaffold

---
+++ src/test/java/com/samialsohan/linkly/LinklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samialsohan.linkly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
